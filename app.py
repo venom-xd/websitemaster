3794c054f4b5631af36cc05020e05845be9d46cb
Aditya Kumar <adityasns77@gmail.com>
2024-10-17 19:32:03 +0530

    python5

--- app.py
+++ app.py
@@ -75,4 +75,5 @@ def add_internship(name, info, link):
     save_internships(internships)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
